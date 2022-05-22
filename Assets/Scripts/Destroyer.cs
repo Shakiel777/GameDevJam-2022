303f30a95783baec401909f6d22de71ad04f8046
David Rood <shakiel@roadrunner.com>
2022-05-22 14:36:11 -0400

    additions

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+
+    public class Destroyer : MonoBehaviour
+    {
+
+        [SerializeField] GameObject targetToDestroy = null;
+
+        public void TargetToDestroy()
+        {
+            Destroy(targetToDestroy);
+        }
+    }
+}
